e6982ece66a005e5ca450a025cee663574066f1e
toshitaka0527 <s-toshitaka-5-2-7@ezweb.ne.jp>
2022-01-07 18:01:51 +0900

    add edit link, edit function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'users/show'
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations"
+  }
+
   root to: "articles#index"
   resources :articles do
     resources :comments, only: [:new, :create, :edit, :update, :destroy]
