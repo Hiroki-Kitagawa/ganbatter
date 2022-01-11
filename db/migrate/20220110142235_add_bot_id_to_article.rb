df17e9f7187d71de70234862fbb12b60be0817b3
Hiroki Kitagawa <mogeetan@outlook.jp>
2022-01-11 11:40:42 +0900

    AddBotIdToArticleを修正

--- db/migrate/20220110142235_add_bot_id_to_article.rb
+++ db/migrate/20220110142235_add_bot_id_to_article.rb
@@ -2,5 +2,5 @@ class AddBotIdToArticle < ActiveRecord::Migration[6.1]
   def change
     add_column :articles, :bot_id, :integer, default: 0
   end
-  add_index :articles, :bot_id
+  # add_index :articles, :bot_id
 end
