e4fee787b8e4015d32d1bc509cc38bc927c5b1eb
naganosinya <cknumber5@gmail.com>
2022-01-13 12:11:06 +0900

    nickname_or_nameを定義してリファクタリング

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -7,6 +7,10 @@ module ArticlesHelper
     User.find_by(id: article.user_id).profile&.nickname
   end
 
+  def nickname_or_name(article)
+    user_nickname(article) || user_name(article)
+  end
+
   def bot_name(article)
     bot = Bot.find_by(id: article.bot_id)
     bot&.bot_name
